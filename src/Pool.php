f33e18ee17d9b480fb0865fcbc6a1c4374b43e0c
walker <showphp@foxmail.com>
2018-05-08 23:42:08 +0800

    修改属性，增加等待队列与空闲连接；修改初始化代码

--- src/Pool.php
+++ src/Pool.php
@@ -1,21 +1,30 @@
 <?php
 namespace mysqlPool;
 
+use Swoole\Mysql\Exception;
+
 class Pool
 {
     protected $maxConn = 100; //最大连接数
     protected $minConn = 5; // 最小连接数
     protected $timeout = 60; // 连接时间
+    protected $maxWaitNum = 100; // 最大等待数
 
     private $poolConnNum = 0; // 连接池连接数
     private $isInited = false; // 是否初始化
-    private $connections = array(); // 连接池
+    private $idlePool; // 空闲连接
+    private $busyPool; // 工作连接
+    private $waitQueue; // 等待队列
+
 
     public static $instance; //连接池对象单例
 
     private function __construct()
     {
-
+        $this->idlePool = new \SplQueue();
+        $this->busyPool = new \SplQueue();
+        $this->waitQueue = new \SplQueue();
+        $this->init();
     }
 
     private function __clone()
@@ -33,26 +42,36 @@ class Pool
         }
     }
 
-    public function run()
-    {
-        if ($this->isInited == false){
-            $this->init();
-        }
-    }
 
     /**
      * 初始化连接池
      */
-    public function init()
+    protected function init()
     {
-        $this->connections = new \SplQueue();
+        if ($this->isInited == false) {
 
-        for($i = 0;$i < $this->minConn;$i++){
-            $db = new Db();
-            $this->poolConnNum++;
-            $this->connections->push($db);
+            for ($i = 0; $i < $this->minConn; $i++) {
+                $db = new Db();
+                $this->poolConnNum++;
+                $this->idlePool->push($db);
+            }
+            $this->isInited = true;
         }
-        $this->isInited = true;
+    }
+
+
+    public function getConnection($serv,$fd)
+    {
+        if ($this->idlePool->count() == 0){
+            // 空闲连接池数为0
+            if ($this->waitQueue->count() < $this->maxWaitNum){
+                $this->waitQueue->push(array($serv,$fd));
+            }else{
+                throw new Exception('wait queue exceed');
+            }
+        }else{
+
+        }
     }
 
 
