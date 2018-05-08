619f9a38a1dcfde652d4cf7cc6676b39c176bd74
walker <showphp@foxmail.com>
2018-05-08 23:02:49 +0800

    删除配置文件，db对象创建；pool对象创建初始化

--- src/Db.php
+++ src/Db.php
@@ -3,5 +3,40 @@ namespace mysqlPool;
 
 class Db
 {
+    const DB_HOST = 'localhost';
+    const DB_PORT = 3306;
+    const DB_NAME = 'test';
+    const DB_USER = 'root';
+    const DB_PWD = 'root';
+
+    private $conn;
+
+    public function __construct()
+    {
+        require_once './config.php';
+        $this->conn = new \PDO(
+            sprintf("mysql:host=%s;port=%d;dbname=%s",self::DB_HOST,self::DB_PORT,self::DB_NAME),
+            self::DB_USER,
+            self::DB_PWD,
+            array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8';",
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_PERSISTENT => true
+            )
+        );
+    }
+
+    public function query($sql,$param)
+    {
+        $this->conn->prepare($sql);
+        return $this->conn->query($param);
+    }
+
+    public function execute($sql,$param)
+    {
+        $this->conn->prepare($sql);
+        return $this->conn->exec($param);
+    }
+
 
 }
--- src/Pool.php
+++ src/Pool.php
@@ -6,11 +6,53 @@ class Pool
     protected $maxConn = 100; //最大连接数
     protected $minConn = 5; // 最小连接数
     protected $timeout = 60; // 连接时间
-    private $conn_count = 0;
-    private $isInited = false; // 是否初始化
 
+    private $poolConnNum = 0; // 连接池连接数
+    private $isInited = false; // 是否初始化
+    private $connections = array(); // 连接池
+
+    public static $instance; //连接池对象单例
+
+    private function __construct()
+    {
+
+    }
+
+    private function __clone()
+    {
+        // TODO: Implement __clone() method.
+    }
+
+    public function getInstance()
+    {
+        if (self::$instance instanceof self){
+            return self::$instance;
+        }else{
+            self::$instance = new self();
+            return self::$instance;
+        }
+    }
+
+    public function run()
+    {
+        if ($this->isInited == false){
+            $this->init();
+        }
+    }
+
+    /**
+     * 初始化连接池
+     */
     public function init()
     {
+        $this->connections = new \SplQueue();
+
+        for($i = 0;$i < $this->minConn;$i++){
+            $db = new Db();
+            $this->poolConnNum++;
+            $this->connections->push($db);
+        }
+        $this->isInited = true;
     }
 
 
